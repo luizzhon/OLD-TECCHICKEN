c5936b5e9f49446fede1819248720718914241ef
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-03-26 18:22:03 -0300

    Simulador

---
+++ SPRINT2/Site/simuladorfinanceiro.js
@@ -0,0 +1,7 @@
+function simular() {
+    var metro_quadrado = Number(input_metroquadrado.value);
+    var qtd_galinhas = Number(input_qtdgalinhas.value);
+    var plano_selecionado = select_planos.value;
+
+
+}
