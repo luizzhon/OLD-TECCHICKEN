846ee34166eedf10ddc533380e4c500e0079c5e3
GustavoQuaresma <gustavo.costa@bandtec.com>
2021-03-31 12:00:02 -0300

    Realizando o Js do login

---
+++ SPRINT2/Site/scriptlogin.js
@@ -0,0 +1,17 @@
+
+
+function login() {
+  var email1 = email.value;
+  var senha1 = senha.value;
+  if (email1 == "" && senha1 == "") {
+    alert("Preencha todos os campos.");
+  }
+  else if (email1 == "granja@gmail.com" && senha1 == "123") {
+    alert("Login Efetivado");
+    window.location.href = "graficos.html";
+    setTimeout("login()", 2500)
+  }
+  else {
+    alert("Email ou senha incorretos.");
+  }
+}
