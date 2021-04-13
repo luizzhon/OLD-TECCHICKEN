da28b37a38c44a6c4bd4906cb916cd1fefa83411
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-04-13 18:10:07 -0300

    atualizei o simulador

--- SPRINT2/Site/simuladorfinanceiro.js
+++ SPRINT2/Site/simuladorfinanceiro.js
@@ -1,7 +1,21 @@
 function simular() {
-    var metro_quadrado = Number(input_metroquadrado.value);
-    var qtd_galinhas = Number(input_qtdgalinhas.value);
-    var plano_selecionado = select_planos.value;
+    var metro_quadrado = Number(in_metroquadrado.value);
+    var qtd_galinhas = Number(in_galinhas.value);
+    
+    var perca_total = (metro_quadrado * 350) + preju_galinhas;
+    var preju_galinhas = qtd_galinhas*30;
+    var economia_plano_mensal = perca_total - 5040;
+    var economia_plano_anual = perca_total - 4200;
 
-
+    quantidade_galinhas.innerHTML = `${qtd_galinhas}`;
+    prejuizo_galinhas.innerHTML = `R$${preju_galinhas.toFixed(2)};`
+    economia_mensal.innerHTML = `R$${economia_plano_mensal.toFixed(2)}`;
+    economia_anual.innerHTML = `R$${economia_plano_anual.toFixed(2)}`;
+    
+    p_total.innerHTML = perca_total.toFixed(2);
 }
+
+// Um arduíno por 50m²
+// Uma galinha por R$30,00
+// Arduíno = R$50,00 + LM35 = R$20,00
+// 350 R$ por m² 
