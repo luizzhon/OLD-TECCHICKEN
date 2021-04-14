6d9d8a9d74c9885ca097b2b479d1b9e7331770bb
BrunnoCastigrini <brunno.castigrini@bandtec.com.br>
2021-04-13 21:30:04 -0300

    att simulador

--- SPRINT2/Site/simuladorfinanceiro.js
+++ SPRINT2/Site/simuladorfinanceiro.js
@@ -1,18 +1,20 @@
 function simular() {
     var metro_quadrado = Number(in_metroquadrado.value);
     var qtd_galinhas = Number(in_galinhas.value);
-    
-    var perca_total = (metro_quadrado * 350) + preju_galinhas;
+   
     var preju_galinhas = qtd_galinhas*30;
+    var perca_total = (metro_quadrado * 350) + preju_galinhas + 1000; 
     var economia_plano_mensal = perca_total - 5040;
     var economia_plano_anual = perca_total - 4200;
+
+    div_resultado.style.display = 'block'
 
     quantidade_galinhas.innerHTML = `${qtd_galinhas}`;
     prejuizo_galinhas.innerHTML = `R$${preju_galinhas.toFixed(2)};`
     economia_mensal.innerHTML = `R$${economia_plano_mensal.toFixed(2)}`;
     economia_anual.innerHTML = `R$${economia_plano_anual.toFixed(2)}`;
     
-    p_total.innerHTML = perca_total.toFixed(2);
+    p_total.innerHTML = `R$${perca_total.toFixed(2)}`;
 }
 
 // Um arduíno por 50m²
